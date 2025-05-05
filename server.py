aaf0a60b2dbbcea60c12720dc7767ac5ab2e946a
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-05 17:58:23 -0600

    Correctly taking in forum data to server

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+import flask as fl
 import os
 
 SAMPLE_INPUT = "Example body, of text"
 SAMPLE_OUTPUT = "Example, of text body!"
 
-app = Flask(__name__)
+app = fl.Flask(__name__)
 
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return fl.render_template("index.html")
 
 
 @app.route("/get-level")
 def getLevel():
     return f'{{"in": "{SAMPLE_INPUT}", "out": "{SAMPLE_OUTPUT}"}}'
+
+
+@app.route("/check-strokes")
+def checkStrokes():
+    strokes = fl.request.args.get("strokes")
+    print(f"Strokes: {strokes}")
+    return "Wrong!"
