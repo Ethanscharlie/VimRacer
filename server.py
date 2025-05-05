f46ccef2a0c30d2035c55fa06592c9970702fb3b
Ethanscharlie <ethanscharlie@protonmail.com>
2025-05-05 12:58:14 -0600

    Working fetch from webserver

--- server.py
+++ server.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template
 import os
 
+SAMPLE_INPUT = "Example body, of text"
+SAMPLE_OUTPUT = "Example, of text body!"
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def main():
     return render_template("index.html")
+
+
+@app.route("/get-level")
+def getLevel():
+    return f'{{"in": "{SAMPLE_INPUT}", "out": "{SAMPLE_OUTPUT}"}}'
